b1b4735548086dcd65935f57bf161e8643621a37
chandrima0503 <chandrimam06@gmail.com>
2022-10-18 12:18:45 +0100

    Greatlearning lab5

--- Spring_MVC/src/main/java/com/greatLearning/DebateRegistration/Controller/HelloController.java
+++ Spring_MVC/src/main/java/com/greatLearning/DebateRegistration/Controller/HelloController.java
@@ -4,11 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
 public class HelloController {
-	@RequestMapping("/hello")
-	public String showMainPage() {
-		return "demo";
+	@RequestMapping("/")
+	public String redirectPage() {
+		return "redirect:/student/list";
 	}
 
 }
